1aa4ac5d54746dffc963b63229c1f35d5c13e56f
Mahmoud_Madih <moodbeder150@gmail.com>
2024-05-09 20:12:45 +0300

    no message

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:rive/rive.dart';
-import 'package:youtube_animated_login/animation_enum.dart';
+
+
+import 'Animation_enum.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
